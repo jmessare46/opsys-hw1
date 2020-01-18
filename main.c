0012690902da80695419561f43e7efe3f5e2c352
Joseph Messare <jmessare46@gmail.com>
2020-01-18 10:08:10 -0500

    Started to read in the file and allocate memory to the cache.

--- main.c
+++ main.c
@@ -1,18 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
 
+// The main function
 int main(int argc, char *argv[]) {
     if(argc != 3)
     {
-        printf("Error: You must provide 2 arguments you provided %d", argc);
-        return 0;
+        fprintf(stderr, "Error: You must provide 2 arguments you provided %d", argc);
+        return 1;
     }
     else
     {
-        for(int x = 0; x < argc; x++)
+        FILE *fp;
+        char *filename = argv[2];
+        char *str = calloc(128, sizeof(char));
+        char **cache = (char**) calloc((unsigned int) argv[1], sizeof(char*));
+
+        fp = fopen(filename, "r");
+        if(fp == NULL)
         {
-            printf("\n%s", argv[x]);
+            fprintf(stderr, "Could not open file %s", filename);
+            return 1;
         }
-        printf("Hello, World!\n");
+        else
+        {
+            int index = 0;
+            while (fgets(str, 128, fp) != NULL)
+            {
+                cache[index] = (char*) calloc(128, sizeof(char*));
+                cache[index] = str;
+                printf("%s", cache[index]);
+                index++;
+            }
+
+            fclose(fp);
+        }
+
+        free(str);
+        free(cache);
         return 0;
     }
 }
