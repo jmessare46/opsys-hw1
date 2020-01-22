66ba97a62cbdcf36c36db7565ef5767e0524156c
Joseph Messare <jmessare46@gmail.com>
2020-01-21 22:45:13 -0500

    Began using the start notation for pointers. Also began reading the file word for word and calculating the ascii sum of each word.

--- main.c
+++ main.c
@@ -1,9 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
-// The main function
-int main(int argc, char *argv[]) {
+/**
+ * Gets the sum of the word based on it's ascii values
+ *
+ * @param word The word to get the sum of
+ *
+ * @return The sum of all of the ascii values of each character in the word
+ */
+int get_ascii_sum(char * word)
+{
+    char * temp;
+    int sum = 0;
+
+    for (temp = word; *temp != '\0'; temp++) {
+        sum += (int) *temp;
+    }
+
+    return sum;
+}
+
+/**
+ * Runs the main function.
+ *
+ * @param argc The number of arguments passed
+ * @param argv An array of all the arguments
+ *
+ * @return Whether or not the program ran successfully
+ */
+int main(int argc, char **argv) {
     if(argc != 3)
     {
         fprintf(stderr, "Error: You must provide 2 arguments you provided %d", argc);
@@ -12,9 +39,10 @@ int main(int argc, char *argv[]) {
     else
     {
         FILE *fp;
-        char *filename = argv[2];
+        char *filename = *(argv+2);
         char *str = calloc(128, sizeof(char));
-        char **cache = (char**) calloc((unsigned int) argv[1], sizeof(char*));
+        int cache_size = atoi(*(argv+1));
+        char **cache = (char**) calloc(cache_size, sizeof(char*));
 
         fp = fopen(filename, "r");
         if(fp == NULL)
@@ -27,9 +55,47 @@ int main(int argc, char *argv[]) {
             int index = 0;
             while (fgets(str, 128, fp) != NULL)
             {
-                cache[index] = (char*) calloc(128, sizeof(char*));
-                cache[index] = str;
-                printf("%s", cache[index]);
+                const char s[2] = " ";
+                char *token;
+                char *cache_value = NULL;
+
+                /* get the first token */
+                token = strtok(str, s);
+                cache_value = *(cache + (int)(get_ascii_sum(token) / cache_size));
+                if(*cache_value == '0')
+                {
+                    *(cache + (int)(get_ascii_sum(token) / cache_size)) = calloc(strlen(token), sizeof(char));
+                    strcpy(*(cache + (int)(get_ascii_sum(token) / cache_size)), token);
+                }
+                else
+                {
+                    realloc(*(cache + (int)(get_ascii_sum(token) / cache_size)), strlen(token) * sizeof(char));
+                    strcpy(*(cache + (int)(get_ascii_sum(token) / cache_size)), token);
+                }
+
+                /* walk through other tokens */
+                while( token != NULL ) {
+                    printf( " %s\n", token );
+
+                    token = strtok(NULL, s);
+                    if(*cache_value == '0')
+                    {
+                        *(cache + (int)(get_ascii_sum(token) / cache_size)) = calloc(strlen(token), sizeof(char));
+                        strcpy(*(cache + (int)(get_ascii_sum(token) / cache_size)), token);
+                    }
+                    else
+                    {
+                        realloc(*(cache + (int)(get_ascii_sum(token) / cache_size)), strlen(token) * sizeof(char));
+                        strcpy(*(cache + (int)(get_ascii_sum(token) / cache_size)), token);
+                    }
+                }
+
+                for(int x = 0; x < cache_size; x++)
+                {
+                    printf("%s\n", *(cache+x));
+                    free(*(cache+x));
+                }
+
                 index++;
             }
 
@@ -41,3 +107,4 @@ int main(int argc, char *argv[]) {
         return 0;
     }
 }
+
